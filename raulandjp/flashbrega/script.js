53c3bd1cb4ed8ddb26905f952ffc126f24dd083d
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2023-02-21 12:34:16 -0300

    gg

--- raulandjp/flashbrega/script.js
+++ raulandjp/flashbrega/script.js
@@ -1,10 +1,25 @@
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const apiUrl = 'https://radiojpp.000webhostapp.com/';
+const API_KEY = 'fcfe44809de84129fab53e785124bb95';
+const MOVIE_ID = 774825;
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
-fetch(proxyUrl + apiUrl)
+// Faz a solicitação AJAX para a API TMDb
+fetch(`https://api.themoviedb.org/3/movie/774825?api_key=fcfe44809de84129fab53e785124bb95&language=pt-BR`)
   .then(response => response.json())
   .then(data => {
-    const songTitle = data.title;
-    document.getElementById('song-title').textContent = songTitle;
+    const movieElement = document.getElementById('movie');
+
+    // Cria o HTML dinamicamente com base nos dados da API
+    const html = `
+      <h1>${data.title}</h1>
+      <p><strong>Gêneros:</strong> ${data.genres.map(genre => genre.name).join(', ')}</p>
+      <img src="${IMAGE_BASE_URL}${data.poster_path}" alt="${data.title} Poster">
+      <p><strong>Sinopse:</strong> ${data.overview}</p>
+      <p><strong>Nota:</strong> ${data.vote_average}</p>
+      <p><strong>Data de lançamento:</strong> ${data.release_date}</p>
+      <p><strong>Duração:</strong> ${data.runtime} minutos</p>
+    `;
+
+    // Adiciona o HTML à página
+    movieElement.innerHTML = html;
   })
   .catch(error => console.error(error));
