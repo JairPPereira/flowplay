132666edbcbcbff227c95080cef99d87f1927db5
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2023-10-19 21:52:11 -0300

    hhh

--- jppassado/server.js
+++ jppassado/server.js
@@ -1,21 +1,18 @@
 const express = require('express');
-const fetch = require('node-fetch');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const app = express();
-const PORT = 3000;
 
-app.use(express.static('public'));
+// Configuração do proxy para a API
+app.use('/api', createProxyMiddleware({
+    target: 'https://zenoplay.zenomedia.com',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/api': '/api',
+    },
+}));
 
-app.get('/api/titulo-musica', async (req, res) => {
-    try {
-        const response = await fetch('https://zenoplay.zenomedia.com/api/zenofm/nowplaying/2cgy8mzvsy8uv');
-        const data = await response.json();
-        res.json(data);
-    } catch (error) {
-        console.error('Erro ao obter dados da API:', error);
-        res.status(500).json({ error: 'Erro ao obter dados da API' });
-    }
-});
-
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+// Iniciar o servidor na porta 3000
+app.listen(3000, () => {
+    console.log('Servidor proxy em execução na porta 3000');
 });
